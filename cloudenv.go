9503c0a3ac8121de3d2a544ab06b9cad49b4258f
Andrew Glen-Young <andrew.glenyoung@datadoghq.com>
2018-05-15 17:26:12 -0400

    Allow cloud providers to be chosen

--- cloudenv.go
+++ cloudenv.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	probeTimeout = 15 // seconds
+	defaultTimeout = 5 // seconds
 )
 
 type CloudConfig struct {
@@ -21,20 +21,52 @@ type cloudprovider interface {
 	probe(r chan *CloudConfig)
 }
 
-func Discover() *CloudConfig {
+type cloudproviders struct {
+	providers []cloudprovider
+}
+
+type provider func(*cloudproviders)
+
+func AWS() provider {
+	return func(c *cloudproviders) {
+		c.providers = append(c.providers, newAWSProvider())
+		return
+	}
+}
+
+func GCP() provider {
+	return func(c *cloudproviders) {
+		c.providers = append(c.providers, newGCPProvider())
+		return
+	}
+}
+
+func Discover(timeout time.Duration, providers ...provider) *CloudConfig {
+	if timeout <= 0 {
+		timeout = defaultTimeout * time.Second
+	}
+
+	cp := new(cloudproviders)
+
+	if len(providers) == 0 {
+		providers = []provider{
+			AWS(),
+			GCP(),
+		}
+	}
+
+	for _, p := range providers {
+		p(cp)
+	}
+
 	r := make(chan *CloudConfig, 1)
 	defer close(r)
 
-	providers := []cloudprovider{
-		newAWSProvider(),
-		newGCPProvider(),
+	for _, p := range cp.providers {
+		p.probe(r)
 	}
 
-	for _, provider := range providers {
-		provider.probe(r)
-	}
-
-	ticker := time.NewTicker(probeTimeout * time.Second)
+	ticker := time.NewTicker(timeout)
 	defer ticker.Stop()
 
 	cfg := new(CloudConfig)
